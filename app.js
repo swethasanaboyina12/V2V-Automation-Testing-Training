78245cd203a1f713e586ca035a43caaf1a441a43
swethasanaboyina12 <124357699+swethasanaboyina12@users.noreply.github.com>
2026-01-12 14:26:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let apple = ["red","fruit","green",50,{name:"letter"}];
+console.log(apple);
+const games = {cricket:"kohli",
+    tennis:"sania",
+    boxing:"mary"};
+    console.log(games);
+var a = 50;
+var b = 70;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log("multi:",a*b);
